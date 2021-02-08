c34e89cbe41615b9974672181ab8534163775f6a
Arya Aditiya <aryaaditiya15@gmail.com>
2021-02-08 16:38:41 +0700

    benerin rv pasien

--- app/src/main/java/ac/id/pradita/klinikhijauputih/PasienActivity.java
+++ app/src/main/java/ac/id/pradita/klinikhijauputih/PasienActivity.java
@@ -1,29 +1,26 @@
 package ac.id.pradita.klinikhijauputih;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.Query;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import ac.id.pradita.klinikhijauputih.adapter.PasienAdapter;
 import ac.id.pradita.klinikhijauputih.model.Pasien;
 
 public class PasienActivity extends AppCompatActivity {
 
     RecyclerView rv_pasien;
-    List<Pasien> pasienList;
-    PasienAdapter adapter;
     DatabaseReference reference;
 
     @Override
@@ -33,30 +30,53 @@ public class PasienActivity extends AppCompatActivity {
 
         rv_pasien = findViewById(R.id.rvPasien);
 
-        rv_pasien.setHasFixedSize(true);
         rv_pasien.setLayoutManager(new LinearLayoutManager(this));
 
-        pasienList = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference();
 
-        reference = FirebaseDatabase.getInstance().getReference("Pasien");
-
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot ds:snapshot.getChildren()){
-                    Pasien data = ds.getValue(Pasien.class);
-                    pasienList.add(data);
-                }
-                adapter = new PasienAdapter(pasienList);
-                rv_pasien.setAdapter(adapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        getListData();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getListData();
+    }
 
+    private void getListData() {
+        Query mQuery = reference.child("Pasien");
+        mQuery.keepSynced(true);
+
+        FirebaseRecyclerAdapter<Pasien, ListPasienViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Pasien, ListPasienViewHolder>(
+                Pasien.class, R.layout.item_pasien, ListPasienViewHolder.class, mQuery) {
+            @Override
+            protected void populateViewHolder(ListPasienViewHolder listPasienViewHolder, Pasien pasien, int i) {
+                listPasienViewHolder.setNamaPasien(pasien.getNama());
+
+                listPasienViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // tulis intent disini bwang
+
+                    }
+                });
+            }
+        };
+        rv_pasien.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class ListPasienViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        TextView tvNamaPasien;
+
+        public ListPasienViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        void setNamaPasien(String nama_pasien) {
+            tvNamaPasien = mView.findViewById(R.id.nama_pasien);
+            tvNamaPasien.setText(nama_pasien);
+        }
+    }
 }
