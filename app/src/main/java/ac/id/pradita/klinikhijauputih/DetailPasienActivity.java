9a095a73512d4f2172c55d98108618225b67dc93
Fariqofficial <fariqn.diaz22@gmail.com>
2021-02-09 12:00:14 +0700

    Bug Detail Pasien

--- app/src/main/java/ac/id/pradita/klinikhijauputih/DetailPasienActivity.java
+++ app/src/main/java/ac/id/pradita/klinikhijauputih/DetailPasienActivity.java
@@ -1,16 +1,84 @@
 package ac.id.pradita.klinikhijauputih;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ac.id.pradita.klinikhijauputih.model.Pasien;
+
 
 public class DetailPasienActivity extends AppCompatActivity {
-    EditText no_ktp, nama_pasien, alamat_rumah, umur, no_telp, nama_ibu, nama_pasangan;
+
+    TextView tvKTP, tvNama, tvAlamat, tvJenkel, tvUmur, tvStatus, tvTelpon, tvIbuPasien, tvPasangan;
+    ProgressDialog dialog;
+    Button edit, hapus;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_pasien);
+
+        tvKTP = findViewById(R.id.ktpPasien);
+        tvNama = findViewById(R.id.namaPasien);
+        tvAlamat = findViewById(R.id.alamatPasien);
+        tvJenkel = findViewById(R.id.jenkel);
+        tvUmur = findViewById(R.id.umur);
+        tvStatus = findViewById(R.id.status);
+        tvTelpon = findViewById(R.id.telpPasien);
+        tvIbuPasien = findViewById(R.id.ibuPasien);
+        tvPasangan = findViewById(R.id.pasanganPasien);
+        edit = findViewById(R.id.editDtlPasien);
+        hapus = findViewById(R.id.hapusDtlPasien);
+
+        dialog = new ProgressDialog(this);
+        reference = FirebaseDatabase.getInstance().getReference("Pasien");
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditPasienActivity.class);
+         //       intent.putExtra();
+            }
+        });
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Pasien pasien = snapshot.getValue(Pasien.class);
+
+                tvNama.setText(pasien.getNama());
+                tvKTP.setText(pasien.getNo_ktp());
+                tvAlamat.setText(pasien.getAlamat());
+                tvJenkel.setText(pasien.getJenis_kelamin());
+                tvUmur.setText(pasien.getUmur());
+                tvStatus.setText(pasien.getStatus());
+                tvTelpon.setText(pasien.getNo_hp());
+                tvIbuPasien.setText(pasien.getNama_ibu());
+                tvPasangan.setText(pasien.getNama_pasangan());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Maaf Terjadi Kesalahan, Silahkan Coba Kembali",Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
     }
+
 }
--- app/src/main/java/ac/id/pradita/klinikhijauputih/EditPasienActivity.java
+++ app/src/main/java/ac/id/pradita/klinikhijauputih/EditPasienActivity.java
@@ -1,14 +1,94 @@
 package ac.id.pradita.klinikhijauputih;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ac.id.pradita.klinikhijauputih.model.Pasien;
 
 public class EditPasienActivity extends AppCompatActivity {
+    EditText etNamaPasien, etNomorKtpPasien, etAlamatPasien, etUmurPasien, etTelpPasien, etNamaIbuPasien, etNamAPasanganPasien;
+    DatabaseReference reference;
+    ProgressDialog dialog;
+    FirebaseUser user;
+    Button btnEdit;
+    Spinner edtJenkel, edtStatus;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_pasien);
+
+        etNomorKtpPasien = findViewById(R.id.editKtpPasien);
+        etNamaPasien = findViewById(R.id.editNamaPasien);
+        etAlamatPasien = findViewById(R.id.edtAlamatPasien);
+        edtJenkel = findViewById(R.id.edtSpinnerJenkel);
+        etUmurPasien = findViewById(R.id.edtUmur);
+        edtStatus = findViewById(R.id.edtSpinnerStatus);
+        etNamaIbuPasien = findViewById(R.id.edtIbuPasien);
+        etNamAPasanganPasien = findViewById(R.id.edtPasanganPasien);
+        btnEdit = findViewById(R.id.editDataPasien);
+
+        dialog = new ProgressDialog(this);
+        
+        getDataPasien();
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editDataPasien(etNomorKtpPasien.getText().toString(), etNamaPasien.getText().toString(), etAlamatPasien.getText().toString(),
+                        edtJenkel.getSelectedItem().toString(), etUmurPasien.getText().toString(), edtStatus.getSelectedItem().toString(),
+                        etNamaIbuPasien.getText().toString(), etNamAPasanganPasien.getText().toString());
+            }
+        });
+
+    }
+
+    private void editDataPasien(String edit_ktp, String edit_nama, String edit_alamat, String edit_jenkel, String edit_umur, String edit_status,
+                                String edit_namaIbu, String edit_pasangan) {
+
+    }
+
+    private void getDataPasien() {
+        dialog.show();
+        dialog.setMessage("Mohon Tunggu..");
+        FirebaseDatabase.getInstance().getReference("Pasien").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Pasien pasien = snapshot.getValue(Pasien.class);
+
+                etNomorKtpPasien.setText(pasien.getNo_ktp());
+                etNamaPasien.setText(pasien.getNama());
+                etAlamatPasien.setText(pasien.getAlamat());
+                etUmurPasien.setText(pasien.getUmur());
+                etNamaIbuPasien.setText(pasien.getNama_ibu());
+                etNamAPasanganPasien.setText(pasien.getNama_pasangan());
+
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Maaf Terjadi Kesalahan, Silahkan Coba Kembali",Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
     }
 }
